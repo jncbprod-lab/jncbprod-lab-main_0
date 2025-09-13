a326db391ea029a6425711ebeba2b6042b6ab936
jncbprod-lab <jncb.prod@gmail.com>
2025-09-13 19:51:44 +0530

    Add files via upload

---
+++ src/main/java/com/price/repository/PriceAuditTrialRepository.java
@@ -0,0 +1,9 @@
+package com.price.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.price.entity.PriceAuditTrial;
+
+public interface PriceAuditTrialRepository extends CrudRepository<PriceAuditTrial, Long>{
+
+}
