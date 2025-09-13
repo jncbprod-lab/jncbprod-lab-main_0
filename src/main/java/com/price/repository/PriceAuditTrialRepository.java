fbbeaada9f849ae72f4da599e2307ae53c860371
jncbprod-lab <jncb.prod@gmail.com>
2025-09-13 19:54:44 +0530

    Delete src directory

--- src/main/java/com/price/repository/PriceAuditTrialRepository.java
+++
@@ -1,9 +0,0 @@
-package com.price.repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.price.entity.PriceAuditTrial;
-
-public interface PriceAuditTrialRepository extends CrudRepository<PriceAuditTrial, Long>{
-
-}
